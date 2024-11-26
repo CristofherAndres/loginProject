7791914090b437caa122dd39554afe30f176cf0b
Cristofher <Cristofher@gmail.com>
2024-11-26 09:19:33 -0300

    Login con Admin SB2

--- loginProject/urls.py
+++ loginProject/urls.py
@@ -20,9 +20,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+from usuariosApp.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")), # Todas las urls de autenticación
-    path("", TemplateView.as_view(template_name="index.html"), name="home"),
+    path("", index, name="home"),
     path("usuarios/", include("usuariosApp.urls"))
 ]
